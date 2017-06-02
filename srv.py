4e9e9468869bafa5a64249f6f218ef1d4c4cff0f
abby.embree <spacerainbow000@users.noreply.github.com>
2017-06-02 12:40:17 -0700

    less terrible logging

--- srv.py
+++ srv.py
@@ -2,6 +2,7 @@ import sys
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
+import datetime
 import time, threading
 
 import subprocess
@@ -13,14 +14,18 @@ Protocol     = "HTTP/1.0"
 port = 65000
 server_address = ('127.0.0.1', port)
 
-HandlerClass.protocol_version = Protocol
-httpd = ServerClass(server_address, HandlerClass)
+class OVERRIDELOG(SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+        open("log", "a").write("%s - - [%s] %s\n" % (self.address_string(), self.log_date_time_string(),format%args))
+
+httpd = ServerClass(server_address, OVERRIDELOG)
 
 sa = httpd.socket.getsockname()
-print "Serving HTTP on", sa[0], "port", sa[1], "..."
+print "Serving HTTP on" + str(sa[0]) + "port" + str(sa[1]) + "..."
 
 def updateopentickets():
-    subprocess.call("bash DOAPITOUCHES", shell=True)
+    print "pulled times at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    subprocess.call("bash DOAPITOUCHES >> log", shell=True)
     threading.Timer(120, updateopentickets).start()
 
 updateopentickets()
