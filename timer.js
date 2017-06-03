63d57d302f84d567dc6da23dbab8d3c09ac60f4e
Justin Phang <jphang@jamasoftware.com>
2017-06-03 08:41:19 -0700

    moving inline styles over

--- timer.js
+++ timer.js
@@ -3,7 +3,7 @@ var ticketCount = {};
 function createTimer(duration, element) {
 
     var timer = duration, minutes, seconds;
-        var sec_num = parseInt(duration, 10);
+    var sec_num = parseInt(duration, 10);
     hours   = Math.floor(sec_num / 3600);
     minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     seconds = sec_num - (hours * 3600) - (minutes * 60);
@@ -11,31 +11,31 @@ function createTimer(duration, element) {
     element.textContent = hours + ":" + minutes + ":" + seconds;
 
     setInterval(function () {
-                sec_num -= 1;
-                if (sec_num <= 0)
-                        element.textContent = "BREACHED!!!"
-                else
-                {
-                        hours   = Math.floor(sec_num / 3600);
-                        minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-                        seconds = sec_num - (hours * 3600) - (minutes * 60);
-                        element.textContent = hours + ":" + minutes + ":" + seconds;
-                }
+        sec_num -= 1;
+        if (sec_num <= 0)
+            element.textContent = "BREACHED!!!"
+        else
+        {
+            hours   = Math.floor(sec_num / 3600);
+            minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+            seconds = sec_num - (hours * 3600) - (minutes * 60);
+            element.textContent = hours + ":" + minutes + ":" + seconds;
+        }
     }, 1000);
 }
 
 function writeTable(count) {
     table = document.querySelector('#tableprint');
-    var myTable="<table width='100%' border='1'><tr><td width='50%' style='margin: 0px; color: red;'>TICKET NUMBER</td>";
-    myTable+= "<td width='50%' style='margin: 0px; color: red; text-align: center;'>SLA TIME</td>";
+    var myTable ="<table><tr><th class='ticket_num'>TICKET NUMBER</th>";
+    myTable+= "<th class='sla'>SLA TIME</th>";
     myTable+="</tr>";
 
     for (i = 0; i < count; i++) {
         myTable+="<tr>";
-        myTable+="<td style='width: 100px;'><span id='number";
+        myTable+="<td><span id='number";
         myTable+=i;
         myTable+="'></span></td>";
-        myTable+="<td style='width: 100px;'><span id='time";
+        myTable+="<td><span id='time";
         myTable+=i;
         myTable+="'></span></td>";
         myTable+="</tr>";
@@ -68,19 +68,19 @@ function populateTable() {
 }
 
 function formatDate(time) {
-        utime=Date.parse(time)/1000;
-        ctime= + Date;
-        ctime=Math.round((new Date()).getTime() / 1000);
-        time=utime-ctime;
-        time=time-25200; //HARDCODED TIMESHIFT FOR PDT
-        return time;
+    utime=Date.parse(time)/1000;
+    ctime= + Date;
+    ctime=Math.round((new Date()).getTime() / 1000);
+    time=utime-ctime;
+    time=time-25200; //HARDCODED TIMESHIFT FOR PDT
+    return time;
 }
 
 function fullReload() {
     reloadIFrame();
     ticketCount.count=readCount();
     writeTable(ticketCount.count);
-        populateTable();
+    populateTable();
 }
 
 window.onload = function () {
