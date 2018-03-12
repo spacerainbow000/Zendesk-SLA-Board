d22ac999587200552d032f78c5a8fa6ff5624c40
abby.embree <spacerainbow000@users.noreply.github.com>
2018-03-12 16:14:19 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
+
+
 # install dependencies
-
 # install dev tools
-
 if [[ $1 != "--override" ]];
 then
     command -v yum >/dev/null 2>&1 && { #rhel-like
         echo "installing dev tools with yum"
-        yum -y install python
-        yum -y install "Development Tools"
-        yum -y install autoconf automake libtool python-devel
+        sudo yum -y install python
+        sudo yum -y install "Development Tools"
+        sudo yum -y install autoconf automake libtool python-devel
     } || command -v apt >/dev/null 2>&1 && { #debian-like
         echo "installing dev tools with apt"
         apt -y install python
         apt -y install build-essential
         apt -y install autoconf automake libtool python-dev
-    } || echo "neither apt nor yum found; run the steps from this script manually instead. run with --override to override this check." && cat $0 && exit 1
+    } || echo "initial dev tool installation failed; run the steps from this script manually instead. run with --override to override this check." ; cat $0 ; exit 1
 fi
 
 # install pip
